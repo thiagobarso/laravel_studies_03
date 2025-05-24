e93fc4ec7ffe7034653676397735a55fe1cc213c
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-05-24 20:16:45 -0300

    118. Componentes com Vários Slots

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,23 @@
            <x-card-pessoa :$pessoa :$linguas />
         @endforeach
     </div> --}}
-    <div>
+    {{-- <div>
         <h4 class="text-info">Como funciona um Slot?</h4>
         <x-other-card>
             <h4 class="text-danger">Este é o Slot!</h4>
             <h4 class="text-danger">Este é o Slot!</h4>
             <h4 class="text-danger">Este é o Slot!</h4>
         </x-other-card>
-    </div>
+    </div> --}}
+    <x-multi-slot>
+        <x-slot:title>Este é o título</x-slot>
+        <x-slot:content>Este é o conteúdo</x-slot>
+        <x-slot:footer>
+            <ul>
+                <li>Item1</li>
+                <li>Item2</li>
+                <li>Item3</li>
+            </ul>
+        </x-slot>
+    </x-multi-slot>
 @endsection
