9e46014c4193865dd2b8b079ad7286d7e8ea19d1
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-05-24 20:09:59 -0300

    117. Intrpodução aos Slots

---
+++ resources/views/components/other-card.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h3>Este é o texto inicial do component</h3>
+    
+    {{-- vamor exibir o conteúdo do slot aqui com a diretiva blade --}}
+    {{ $slot }}
+    
+    <h3>Este é o texto inicial do component</h3>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,16 @@
-@extends('layouts.main_layout');
+@extends('layouts.main_layout')
 @section('content')
-    <div class="text-center">
+    {{-- <div class="text-center">
         @foreach ($pessoas_linguas as $pessoa => $linguas)
            <x-card-pessoa :$pessoa :$linguas />
         @endforeach
+    </div> --}}
+    <div>
+        <h4 class="text-info">Como funciona um Slot?</h4>
+        <x-other-card>
+            <h4 class="text-danger">Este é o Slot!</h4>
+            <h4 class="text-danger">Este é o Slot!</h4>
+            <h4 class="text-danger">Este é o Slot!</h4>
+        </x-other-card>
     </div>
 @endsection
